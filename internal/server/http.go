2d94406c5f831022f3b63ea84116ea5fb81121e7
Mizan Mahi <49094616+mizanmahi@users.noreply.github.com>
2025-12-24 11:11:25 +0600

    Add comment for root composition guidance in HTTP server setup

--- internal/server/http.go
+++ internal/server/http.go
@@ -7,6 +7,8 @@ import (
 	"gorm.io/gorm"
 )
 
+// root composition can be done here or in main.go based on the complexity of the application.
+
 func StartHTTPServer(e *echo.Echo, db *gorm.DB) {
 
 	user.RegisterRoutes(e, db)
